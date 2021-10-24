b2c34a92bbf15c08726191788174dfdc4d4685f5
375297158888 <newatlant@tut.by>
2021-10-24 21:32:20 +0300

    24/10

---
+++ src/com/company/Zodiac.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Zodiac {
+    private static int count = 13;
+    private static String[] signs = {"Aries", "Taurus", "Gemini", "Cancer","Leo","Virgo","Libra","Scorpio","Sagittarius","Capricorn","Capricorn","Aquarius","Pisces"};
+    private static String[][] dates = {{"21.03", "20.04"}, {"21.04", "21.05"},{"22.05", "21.06"},
+            {"22.06", "22.07"}, {"23.07", "22.08"},{"23.08", "23.09"},
+            {"24.09", "23.10"}, {"24.10", "22.11"},{"23.11", "21.12"},
+            {"1.01", "20.01"},{"22.12", "31.12"}, {"21.01", "19.02"},{"20.02", "20.03"}};
+    public static void main(String[] args) {
+
+    }
+}
